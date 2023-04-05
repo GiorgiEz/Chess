d243a25ca6db5d683065c4634d4dba1592c27b08
GiorgiEz <lipartelianige@gmail.com>
2023-04-05 16:39:26 +0400

    fixed issues

--- src/ChessBoard/ChessBoard.tsx
+++ src/ChessBoard/ChessBoard.tsx
@@ -1,6 +1,4 @@
-import React, {useRef, useEffect} from 'react';
-import './ChessBoard.css';
-import {MovePieces} from "./MovePieces"
+import React from 'react';
 
 import whitePawn from '../assets/white-pawn.png';
 import whiteRook from '../assets/white-rook.png';
@@ -14,65 +12,41 @@ import blackQueen from '../assets/black-queen.png';
 import blackKnight from '../assets/black-knight.png';
 import blackKing from '../assets/black-king.png';
 import blackBishop from '../assets/black-bishop.png';
+import {MovePieces} from "./MovePieces";
 
-interface Props {
-    src: string, x: number, y: number;
+interface Piece {
+    src: string,
+    x: number,
+    y:number,
+    color: "white" | "black",
+    name: string
 }
 
 export const ChessBoard = () => {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const canvasSize = 600
     const squareSize = 75
+    const imageSize = 50
+    const pieces: Piece[] = []
 
-    const pieces: Props[] = [
-        {src: blackRook, x:12.5, y:12.5},
-        {src: blackKnight, x:87.5, y:12.5},
-        {src: blackBishop, x:162.5, y:12.5},
-        {src: blackQueen, x:237.5, y:12.5},
-        {src: blackKing, x:312.5, y:12.5},
-        {src: blackBishop, x:387.5, y:12.5},
-        {src: blackKnight, x:462.5, y:12.5},
-        {src: blackRook, x:537.5, y:12.5},
+    const blackPieces = [blackRook, blackKnight, blackBishop, blackQueen, blackKing, blackBishop, blackKnight, blackRook]
+    const whitePieces = [whiteRook, whiteKnight, whiteBishop, whiteQueen, whiteKing, whiteBishop, whiteKnight, whiteRook]
+    const namesArray = ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"]
 
-        {src: whiteRook, x:12.5, y:537.5},
-        {src: whiteKnight, x:87.5, y:537.5},
-        {src: whiteBishop, x:162.5, y:537.5},
-        {src: whiteQueen, x:237.5, y:537.5},
-        {src: whiteKing, x:312.5, y:537.5},
-        {src: whiteBishop, x:387.5, y:537.5},
-        {src: whiteKnight, x:462.5, y:537.5},
-        {src: whiteRook, x:537.5, y:537.5},
-        ]
-
-    for (let i = 12.5; i < 600; i += squareSize){
-        pieces.push({src: blackPawn, x: i, y:87.5})
-        pieces.push({src: whitePawn, x: i, y:462.5})
+    for (let i = 12.5, index = 0; index < blackPieces.length; i += squareSize, index++) {
+        pieces.push({src: blackPieces[index], x: i, y: 12.5, color: "black", name: namesArray[index]})
+        pieces.push({src: whitePieces[index], x: i, y: 537.5, color: "white", name: namesArray[index]})
+        pieces.push({src: blackPawn, x: i, y: 87.5, color: "black", name:"pawn"})
+        pieces.push({src: whitePawn, x: i, y: 462.5, color: "white", name:"pawn"})
     }
-
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        const ctx = canvas?.getContext('2d');
-        if (ctx) {
-            ctx.clearRect(0, 0, 500, 500);
-            for (let row = 0; row < 8; row++) {
-                for (let col = 0; col < 8; col++) {
-                    const x = col * squareSize;
-                    const y = row * squareSize;
-                    ctx.fillStyle = (row + col) % 2 === 0 ? '#f0d9b5' : '#b58863';
-                    ctx.fillRect(x, y, squareSize, squareSize);
-                }
-            }
-        }
-    }, []);
 
     return (
         <div>
-            <canvas
-                className={"chessboard"}
-                ref={canvasRef}
-                width={600}
-                height={600}
+            <MovePieces
+                pieces={pieces}
+                canvasSize={canvasSize}
+                squareSize={squareSize}
+                imageSize={imageSize}
             />
-            <MovePieces pieces={pieces} />
         </div>
-    );
-};
+    )
+}
--- src/ChessBoard/MovePieces.tsx
+++ src/ChessBoard/MovePieces.tsx
@@ -1,24 +1,38 @@
 import React from "react";
+import "./ChessBoard.css"
 
 interface Props {
-    pieces: {src: string, x: number, y: number}[];
+    pieces: {src: string, x: number, y:number, color: "white" | "black", name: string}[];
+    squareSize: number;
+    canvasSize: number;
+    imageSize: number;
 }
 
 interface State {
     positions: { x: number; y: number }[];
-    isDragging: boolean;
-    draggingIndex: number;
-    mousePosition: { x: number; y: number; };
 }
 
 export class MovePieces extends React.Component<Props, State> {
     canvasRef = React.createRef<HTMLCanvasElement>();
+    pieceImages: HTMLImageElement[] = []
+    isDragging: boolean = false
+    draggingIndex: number = -1
+    mousePosition = {x: 0, y: 0}
+    initialPositions = this.props.pieces.map(({x, y}) => ({x,y}))
+    colorAndNameArray = this.props.pieces.map(({color, name}) => ({name, color}))
+
+    public constructor(props: Props) {
+        super(props);
+
+        for (let i = 0; i < this.props.pieces.length; i++) {
+            const image = new Image();
+            image.src = this.props.pieces[i].src;
+            this.pieceImages.push(image)
+        }
+    }
 
     state: State = {
         positions: this.props.pieces.map(({x,y}) => ({ x, y })),
-        isDragging: false,
-        draggingIndex: -1,
-        mousePosition: {x: 0, y: 0},
     }
 
     componentDidMount() {
@@ -43,22 +57,25 @@ export class MovePieces extends React.Component<Props, State> {
         return {
             x: event.clientX - left,
             y: event.clientY - top,
-        };
+        }
     }
 
     onMouseDown = (event: React.MouseEvent<HTMLCanvasElement>) => {
         const mousePositions = this.getMousePositions(event)
         const index = this.getIndexAtPosition(mousePositions.x, mousePositions.y);
         if (index !== -1) {
-            this.setState({ isDragging: true, draggingIndex: index });
+            this.isDragging = true
+            this.draggingIndex = index
+            this.initialPositions[index] = { ...this.state.positions[index] };
             window.addEventListener("pointermove", this.onMouseMove);
             window.addEventListener("pointerup", this.onMouseUp);
-            this.setState({ mousePosition: { x: mousePositions.x, y: mousePositions.y } });
+            this.mousePosition = { x: mousePositions.x, y: mousePositions.y };
         }
     }
 
     onMouseUp = () => {
-        this.setState({ isDragging: false, draggingIndex: -1 });
+        this.isDragging = false
+        this.draggingIndex = -1
         window.removeEventListener("pointermove", this.onMouseMove);
         window.removeEventListener("pointerup", this.onMouseUp);
     }
@@ -68,32 +85,51 @@ export class MovePieces extends React.Component<Props, State> {
         let x = mousePositions.x
         let y = mousePositions.y
         //Can't move pieces outside of canvas
-        if (x >= 550) x = 550
+        const line = this.props.canvasSize - this.props.imageSize
+        if (x >= line) x = line
         if (x <= 0) x = 0
         if (y <= 0) y = 0
-        if (y >= 550) y = 550
-        this.setState({ mousePosition: { x: x, y: y } });
+        if (y >= line) y = line
+        this.mousePosition = { x: x, y: y };
     }
 
     onMouseMove = (event: MouseEvent) => {
         const mousePositions = this.getMousePositions(event)
         let x = mousePositions.x
         let y = mousePositions.y
+        const i = this.draggingIndex
         // Update position of the piece to be in the middle of the square
-        for (let xPosition = 0; xPosition < 600; xPosition+=75){
-            for (let yPosition = 0; yPosition < 600; yPosition+=75){
-                if ((this.state.mousePosition.x >= xPosition && this.state.mousePosition.x <= xPosition + 75) &&
-                    (this.state.mousePosition.y >= yPosition && this.state.mousePosition.y <= yPosition + 75)){
-                        x = xPosition + 12.5
-                        y = yPosition + 12.5
+        for (let xPos = 0; xPos < this.props.canvasSize; xPos += this.props.squareSize){
+            for (let yPos = 0; yPos < this.props.canvasSize; yPos += this.props.squareSize){
+                if (this.mousePosition.x >= xPos &&
+                    this.mousePosition.x <= xPos + this.props.squareSize &&
+                    this.mousePosition.y >= yPos &&
+                    this.mousePosition.y <= yPos + this.props.squareSize){
+                        x = xPos + 12.5
+                        y = yPos + 12.5
                 }
             }
         }
-        // Only update the state if the mouse has been moved
-        if (x !== this.state.mousePosition.x || y !== this.state.mousePosition.y) {
+        if (this.draggingIndex !== -1) {
+            if (this.colorAndNameArray[i].color === "white" && this.colorAndNameArray[i].name === "pawn") {
+                let initialPosXStart = this.state.positions[i].x - 12.5
+                let initialPosXEnd = this.state.positions[i].x + 62.5
+                let initialPosYStart = this.state.positions[i].y - 12.5
+                let initialPosYEnd = this.state.positions[i].y + 62.5
+                console.log(initialPosYStart, initialPosYEnd)
+                if (!(initialPosXStart <= x && x <= initialPosXEnd) || y >= initialPosYStart+25){
+                    x = this.initialPositions[i].x;
+                    y = this.initialPositions[i].y;
+                }
+            }
+        }
+
+        //Only update the state if the mouse has been moved
+        if (x !== this.mousePosition.x || y !== this.mousePosition.y) {
             const positions = [...this.state.positions];
-            positions[this.state.draggingIndex] = { x, y };
-            this.setState({ positions, mousePosition: { x, y } });
+            positions[this.draggingIndex] = { x, y };
+            this.setState({ positions });
+            this.mousePosition = { x, y }
         }
     }
 
@@ -101,34 +137,50 @@ export class MovePieces extends React.Component<Props, State> {
         const positions = this.state.positions;
         for (let i = 0; i < positions.length; i++) {
             const { x: imageX, y: imageY } = positions[i];
-            const imageWidth = 50;
-            const imageHeight = 50;
-            if (x >= imageX && x <= imageX + imageWidth && y >= imageY && y <= imageY + imageHeight) return i;
+            const imageSize = this.props.imageSize;
+            if (x >= imageX && x <= imageX + imageSize && y >= imageY && y <= imageY + imageSize) return i;
         }
         return -1;
     }
 
     draw_pieces = (ctx: CanvasRenderingContext2D) => {
-        ctx.clearRect(0, 0, 600, 600);
-
+        ctx.clearRect(0, 0, this.props.canvasSize, this.props.canvasSize);
+        const squareSize = this.props.squareSize
+        //Draw background
+        if (ctx) {
+            for (let row = 0; row < 8; row++) {
+                for (let col = 0; col < 8; col++) {
+                    const x = col * squareSize;
+                    const y = row * squareSize;
+                    ctx.fillStyle = (row + col) % 2 === 0 ? "#FFFFFF" : '#000000';
+                    ctx.fillRect(x, y, squareSize, squareSize);
+                }
+            }
+        }
+        //Draw pieces
         for (let i = 0; i < this.props.pieces.length; i++) {
-            const image = new Image();
-            image.src = this.props.pieces[i].src;
+            const image = this.pieceImages[i];
             let { x, y } = this.state.positions[i];
 
             // If the piece is being dragged, draw it at the current mouse position
-            if (this.state.isDragging && i === this.state.draggingIndex) {
-                x = this.state.mousePosition.x;
-                y = this.state.mousePosition.y;
+            if (this.isDragging && i === this.draggingIndex) {
+                x = this.mousePosition.x;
+                y = this.mousePosition.y;
             }
-            ctx.drawImage(image, x, y, 50, 50);
+            ctx.drawImage(image, x, y, this.props.imageSize, this.props.imageSize);
         }
         requestAnimationFrame(() => this.draw_pieces(ctx));
     }
 
     render() {
         return (
-            <canvas ref={this.canvasRef} width={600} height={600} onMouseDown={this.onMouseDown}/>
+            <canvas
+                className={"chessboard"}
+                ref={this.canvasRef}
+                width={this.props.canvasSize}
+                height={this.props.canvasSize}
+                onMouseDown={this.onMouseDown}
+            />
         )
     }
 }
